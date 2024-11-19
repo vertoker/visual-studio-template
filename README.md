efe0daa856371844b01ce0d029e96838fce0d9cf
Kostya <vertogpro@gmail.com>
2024-11-19 13:59:55 +0500

    link glfw as static lib

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Visual Studio C++ Template
 My personal C++ template of new Visual Studio projects on C++
 Tested on Visual Studio 2022
+
+Resources
+- https://youtu.be/3X4DmBfjy0Y?si=HXuK3lSZrxzHk3f3
+- https://youtu.be/or1dAmUO8k0?si=PctGdNy8vCh6V7Bs
+- https://stackoverflow.com/questions/23882112/how-to-add-static-libraries-to-a-visual-studio-project
+- https://www.glfw.org/documentation.html
--- TemplateSolution/src/Main.cpp
+++ TemplateSolution/src/Main.cpp
@@ -1,10 +1,36 @@
 
 #include <iostream>
 #include <fmt/core.h>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* createWindow() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "test window", NULL, NULL);
+	return window;
+}
+
+void destroyWindow(GLFWwindow* window) {
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
 
 int main()
 {
 	std::cout << "hi c" << std::endl;
 	fmt::print("hi fmt\n");
-	std::cin.ignore();
+
+	auto window = createWindow();
+
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	destroyWindow(window);
 }
